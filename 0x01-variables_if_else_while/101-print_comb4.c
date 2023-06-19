28aec90546a1ca6b4cd01c640820d98cd7bb94cd
Hicham <rouijal.hicham@gmail.com>
2023-06-18 20:37:04 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different combinations of three digits
+ *
+ * return: always 0 (Success)
+*/
+
+int main(void)
+{
+
+}
+
